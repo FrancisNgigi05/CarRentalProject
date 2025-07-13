a1a64a4b5cb1161ceb31e5cf95eb453ebc38fa84
toxicanonymous253 <ngugifrancis930@gmail.com>
2025-07-13 10:32:42 +0300

    models.py done and app.py is on progress

---
+++ server/models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from sqlalchemy import String
+
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(10), default='user')
+    
+    rentals = db.relationship('Rental', backref='user', lazy=True)
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(80), default='available')
+    image = db.Column(db.String(255), nullable=True)
+
+    rentals = db.relationship('Rental', backref='car', lazy=True)
+
+
+class Rental(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(20), default='ongoing')
